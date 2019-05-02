f4e932b2ed44ee9674471f5c48f20ae4bd5dfb76
Ramdan <ramdan.firdaus@metrasat.co.id>
2019-05-03 06:56:21 +0700

    Post - postAction creator delete single post test and code

--- src/store/actions/postActions.js
+++ src/store/actions/postActions.js
@@ -12,3 +12,19 @@ export const getUserPosts = (user) => {
       })
   }
 }
+
+export const deletePost = (postId) => {
+  return (dispatch, getState) => {
+    const post = getState().post.postList
+    return axios.delete(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+      .then(response => {
+        if (response.status == 200) {
+          const newPosts = post.filter(post => post.id !== postId)
+          dispatch({
+            type: actionTypes.SET_USER_POSTS,
+            payload: newPosts
+          })
+        }
+      })
+  }
+}
--- src/store/actions/postActions.test.js
+++ src/store/actions/postActions.test.js
@@ -1,20 +1,14 @@
 import moxios from 'moxios'
 
 import { storeFactory } from '../../appUtils'
-import { getUserPosts } from './postActions'
+import { getUserPosts, deletePost } from './postActions'
+import actionTypes from './actionTypes';
 
-describe('getUserPosts action creator', () => {
+describe('post action creator', () => {
+  let postList, store
   beforeEach(() => {
     moxios.install()
-  })
-  afterEach(() => {
-    moxios.uninstall()
-  })
-  test('adds response userPostList to state', () => {
-    const user = {
-      id: 1
-    }
-    const postList = [
+    postList = [
       {
         userId: 1,
         id: 1,
@@ -28,7 +22,15 @@ describe('getUserPosts action creator', () => {
         body: "est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla"
       },
     ]
-    const store = storeFactory()
+    store = storeFactory()
+  })
+  afterEach(() => {
+    moxios.uninstall()
+  })
+  test('adds response userPostList to state when getUserPosts action creator called', () => {
+    const user = {
+      id: 1
+    }
 
     moxios.wait(() => {
       const request = moxios.requests.mostRecent()
@@ -42,6 +44,30 @@ describe('getUserPosts action creator', () => {
       .then(() => {
         const newState = store.getState()
         expect(newState.post.postList).toBe(postList)
+      })
+  })
+  test('delete single post', () => {
+    const deletedPostId = 1
+    let newPostList = postList.filter(post => post.id !== deletedPostId)
+
+    // Mocking user post list
+    store.dispatch({
+      type: actionTypes.SET_USER_POSTS,
+      payload: postList
+    })
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent()
+      request.respondWith({
+        status: 200,
+        response: {}
+      })
+    })
+
+    return store.dispatch(deletePost(deletedPostId))
+      .then(() => {
+        const newState = store.getState()
+        expect(newState.post.postList).toEqual(newPostList)
       })
   })
 })
