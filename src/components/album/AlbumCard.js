dc32a672c49b5dc2c8286db0f5c72984a470da5b
Ramdan <ramdan.firdaus@metrasat.co.id>
2019-05-03 13:39:13 +0700

    Album - AlbumCard component code and test

---
+++ src/components/album/AlbumCard.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Card, Tooltip, Icon } from 'antd'
+import PropTypes from 'prop-types'
+import { withRouter } from 'react-router-dom'
+
+export class NonHocAlbumCard extends React.Component {
+  handlePhotosOfAlbumClick = () => {
+    this.props.history.push(`photos/${this.props.album.id}`)
+  }
+  render() {
+    return (
+      <Card
+        data-test="component-album-card"
+        style={{ width: 300 }}
+        actions={
+          [
+            <Tooltip
+              placement="top"
+              title="View photos"
+            >
+              <Icon
+                type="eye"
+                data-test="photos-of-album"
+                onClick={this.handlePhotosOfAlbumClick}
+              />
+            </Tooltip>
+          ]
+        }
+      >
+        <h4 data-test="album-title">{this.props.album.title}</h4>
+      </Card>
+    )
+  }
+}
+
+NonHocAlbumCard.propTypes = {
+  album: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.title,
+  }).isRequired
+}
+
+export default withRouter(NonHocAlbumCard)
---
+++ src/components/album/AlbumCard.test.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import { findByTestAttr, checkProps } from '../../appUtils'
+import { NonHocAlbumCard } from './AlbumCard'
+
+const defaultProps = {
+  album: {
+    id: 1,
+    title: "quidem molestiae enim",
+  }
+}
+
+const setup = (initialProps={}) => {
+  const setupProps = { ...defaultProps, ...initialProps }
+  return shallow(<NonHocAlbumCard {...setupProps} />)
+}
+
+describe('renders', () => {
+  let wrapper                           
+  beforeEach(() => {
+    wrapper = setup()
+  })
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-album-card')
+    expect(component.length).toBe(1)
+  })
+  test('renders album title', () => {
+    const albumTitle = findByTestAttr(wrapper, 'album-title')
+    expect(albumTitle.text().length).not.toBe(0)
+  })
+  test('renders view detail album button', () => {
+    const componentAlbumCard = findByTestAttr(wrapper, 'component-album-card')
+    const actions = componentAlbumCard.prop('actions')
+    const viewDetailBtn = actions.filter(action => {
+      return action.props.children.props['data-test'] === 'photos-of-album'
+    })
+    expect(viewDetailBtn).not.toBe(undefined)
+  })
+})
+test('does not throw warning with expected props', () => {
+  checkProps(NonHocAlbumCard, defaultProps)
+})
+test('go to photos page when photos of album clicked', () => {
+  const props = {
+    history: [],
+  }
+  const wrapper = setup(props)
+
+  const componentAlbumCard = findByTestAttr(wrapper, 'component-album-card')
+  const actions = componentAlbumCard.prop('actions')
+  const photosOfAlbumBtn = actions.find(action => {
+    return action.props.children.props['data-test'] == 'photos-of-album'
+  })
+  photosOfAlbumBtn.props.children.props['onClick']()
+
+  expect(wrapper.instance().props.history[0]).toBe(`photos/${defaultProps.album.id}`)
+
+})
